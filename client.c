abd1479c2c7f3dbe6d8f9c2809439cac4c479fe8
Alessio C <58214116+AleC-22@users.noreply.github.com>
2023-09-19 22:30:50 +0200

    Update client.c

--- client.c
+++ client.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv){
     }
     
     srand(time(NULL));
-    int secret = rand() % 3000 + 1;
+    int secret = rand() % 300 + 1;
     long int ID = rand();
     fprintf(stdout, "CLIENT %lx SECRET %d\n", ID, secret);
     
@@ -46,7 +46,7 @@ int main(int argc, char **argv){
         isPresent = 0;
         random_k = rand() % S + 1;
 
-        for (int j = 0; j < i; j++){
+        for(int j = 0; j < i; j++){
             if(random_array[j] == random_k){
                 isPresent = 1;
                 break;
@@ -63,7 +63,7 @@ int main(int argc, char **argv){
     struct sockaddr_in address[k];
     int random_index;
     
-    for (int i = 0; i < k; i++){
+    for(int i = 0; i < k; i++){
         if((array_socket_fd[i] = socket(AF_INET, SOCK_STREAM, 0)) < 0){
             perror("SOCKET");
             return 1;
@@ -85,7 +85,7 @@ int main(int argc, char **argv){
     wait_time.tv_sec = secret / 1000;
     wait_time.tv_nsec = (secret % 1000) * 1000000;
     
-    for (int i = 0; i < m; i++){
+    for(int i = 0; i < m; i++){
         random_index = rand() % k;
         if(write(array_socket_fd[random_index], &message, sizeof(message)) < 0){
             perror("WRITE");
